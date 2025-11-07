ad9c55558b8a0c06ed6df90de192c7341d0795e0
ZahwaAnritia <anritia15@gmail.com>
2025-11-07 08:26:20 +0700

    tambah onSubmitBtnClick di FormIsian

---
+++ app/src/main/java/com/example/questnavigastugas_116/view/Formulir.kt
@@ -0,0 +1,10 @@
+package com.example.questnavigastugas_116.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FormIsian(
+    modifier: Modifier = Modifier,
+    onSubmitBtnClick: (String, String, String, String) -> Unit
+) {}
