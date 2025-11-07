d20780bf431a2232edd3bc07a1d5b1016db09da4
ZahwaAnritia <anritia15@gmail.com>
2025-11-07 09:10:22 +0700

    tambah tampilan splash screen

--- app/src/main/java/com/example/questnavigastugas_116/view/SplashScreen.kt
+++ app/src/main/java/com/example/questnavigastugas_116/view/SplashScreen.kt
@@ -1,2 +1,40 @@
 package com.example.questnavigastugas_116.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SplashScreen(
+    onSubmitClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFD8B4FE))
+            .padding(32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Spacer(modifier = Modifier.height(60.dp))
+
+        Text(
+            text = "Selamat Datang",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Medium,
+            color = Color(0xFF6B21A8)
+        )
+
+        Spacer(modifier = Modifier.height(40.dp))
