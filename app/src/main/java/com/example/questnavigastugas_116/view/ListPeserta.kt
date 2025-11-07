55f959637350d46d746d4b96a86cdb5a89f0c958
ZahwaAnritia <anritia15@gmail.com>
2025-11-07 09:36:19 +0700

    tambah composable ListPeserta dengan data peserta

--- app/src/main/java/com/example/questnavigastugas_116/view/ListPeserta.kt
+++ app/src/main/java/com/example/questnavigastugas_116/view/ListPeserta.kt
@@ -1,2 +1,18 @@
 package com.example.questnavigastugas_116.view
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.example.questnavigastugas_116.R
+
+@Composable
+fun ListPeserta(
+    onBackBtnClick: () -> Unit,
+    onHomeBtnClick: () -> Unit,
+    onFormBtnClick: () -> Unit
+) {
+    val items = listOf(
+        Pair(first = stringResource(R.string.nama_lengkap), second = "Dimas Putra"),
+        Pair(first = stringResource(R.string.jenis_kelamin), second = "Laki-laki"),
+        Pair(first = stringResource(R.string.alamat), second = "Yogyakarta"),
+        Pair(first = stringResource(R.string.status_perkawinan), second = "Lajang")
+    )
