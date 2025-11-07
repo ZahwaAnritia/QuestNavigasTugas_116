6b6ce957709abdf2da63c3cc4bf6c875d35995db
ZahwaAnritia <anritia15@gmail.com>
2025-11-07 22:38:19 +0700

    update tampilan

--- app/src/main/java/com/example/questnavigastugas_116/view/SplashScreen.kt
+++ app/src/main/java/com/example/questnavigastugas_116/view/SplashScreen.kt
@@ -1,5 +1,6 @@
 package com.example.questnavigastugas_116.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
@@ -15,7 +17,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -34,38 +38,31 @@ fun SplashScreen(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceBetween
     ) {
-        Spacer(modifier = Modifier.height(60.dp))
+        Spacer(modifier = Modifier.height(100.dp))
 
         Text(
             text = "Selamat Datang",
-            fontSize = 40.sp,
+            fontSize = 35.sp,
             fontWeight = FontWeight.Medium,
+            fontFamily = FontFamily.Monospace,
+            letterSpacing = 1.sp,
             color = Color(0xFF6B21A8)
         )
 
-        Spacer(modifier = Modifier.height(40.dp))
+        Spacer(modifier = Modifier.height(20.dp))
 
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.weight(1f),
             verticalArrangement = Arrangement.Center
         ) {
-            Text(
-                text = "CARD-IST",
-                fontSize = 36.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color(0xFF6B21A8),
-                letterSpacing = 2.sp
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(text = "\uD83D\uDDA5\uFE0F", fontSize = 72.sp)
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(text = "MOBILE APP", fontSize = 14.sp, color = Color(0xFF6B21A8))
-            Text(
-                text = "2025",
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color(0xFF6B21A8)
+            Image(
+                painter = painterResource(id = R.drawable.logo_appp),
+                contentDescription = "App Logo",
+                modifier = Modifier
+                    .height(400.dp)
+                    .width(400.dp)
+                    .padding(bottom = 19.dp)
             )
         }
         Column(
@@ -74,12 +71,14 @@ fun SplashScreen(
         ) {
             Text(
                 text = stringResource(id = R.string.author_name),
-                fontSize = 16.sp,
+                fontSize = 23.sp,
+                fontFamily = FontFamily.Monospace,
                 color = Color(0xFF6B21A8)
             )
             Text(
                 text = stringResource(id = R.string.author_nim),
                 fontSize = 14.sp,
+                fontFamily = FontFamily.Monospace,
                 color = Color(0xFF6B21A8)
             )
         }
