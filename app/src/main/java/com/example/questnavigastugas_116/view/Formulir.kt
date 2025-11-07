fddda66d22f1173b0b458f95d78a36e6c214ded4
ZahwaAnritia <anritia15@gmail.com>
2025-11-07 08:28:50 +0700

    tambah state dan warna di FormIsian

--- app/src/main/java/com/example/questnavigastugas_116/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigastugas_116/view/Formulir.kt
@@ -1,10 +1,29 @@
 package com.example.questnavigastugas_116.view
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun FormIsian(
     modifier: Modifier = Modifier,
     onSubmitBtnClick: (String, String, String, String) -> Unit
-) {}
+) {
+    @androidx.compose.runtime.Composable {
+        var textNama by remember { mutableStateOf("") }
+        var textAlamat by remember { mutableStateOf("") }
+        var textJK by remember { mutableStateOf("") }
+        var textStatus by remember { mutableStateOf("") }
+        var showDialog by remember { mutableStateOf(false) }
+
+        val gender = listOf("Laki-laki", "Perempuan")
+        val statusPerkawinan = listOf("Janda", "Lajang", "Duda")
+
+        val purpleBackground = Color(0xFFE6D5F5)
+        val purpleButton = Color(0xFF7C3AED)
+        val purpleHeader = Color(0xFFB794F6)
+}
